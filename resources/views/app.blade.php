3056a385fae8e89794317c395f16a1168cba5d88
SHIN_NARITA <triplfjump1417@gmail.com>
2017-08-24 14:16:04 +0900

    create views app,index

---
+++ resources/views/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>books management</title>
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:400,300" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+    <sction class="container">
+      @yield('content')
+    </sction>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+  </body>
+</html>
