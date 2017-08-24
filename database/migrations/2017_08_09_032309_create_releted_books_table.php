646965634ab25e02a5ad6cc0de41b98da784edac
SHIN_NARITA <triplfjump1417@gmail.com>
2017-08-24 17:14:37 +0900

    create migrations && seeder

---
+++ database/migrations/2017_08_09_032309_create_releted_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReletedBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('releted_books', function (Blueprint $table)
+        {
+
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+            $table->softDeletes();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('releted_books');
+    }
+}
